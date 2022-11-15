0fc368414b624ba5e5c0414ad264e9b462a1e605
xXSchrandXx <marvinschrand@yahoo.de>
2022-11-15 18:04:11 +0100

    Create Mod pages

---
+++ files/lib/action/KasMailResetListAction.class.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace wcf\action;
+
+use Laminas\Diactoros\Response\EmptyResponse;
+use Laminas\Diactoros\Response\RedirectResponse;
+use wcf\page\KasMailPage;
+use wcf\action\AbstractAction;
+use wcf\system\cache\builder\KasMailCacheBuilder;
+use wcf\system\request\LinkHandler;
+
+final class KasMailResetListAction extends AbstractAction
+{
+    /**
+     * @inheritDoc
+     */
+    public $neededPermission = ['mod.kas.canClearMails'];
+
+    /**
+     * @inheritDoc
+     */
+    public function execute()
+    {
+        parent::execute();
+
+        KasMailCacheBuilder::getInstance()->reset();
+
+        $this->executed();
+
+        if (isset($_POST['noRedirect'])) {
+            return new EmptyResponse();
+        } else {
+            return new RedirectResponse(
+                LinkHandler::getInstance()->getControllerLink(KasMailPage::class)
+            );
+        }
+    }
+}
---
+++ files/lib/page/KasMailPage.class.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace wcf\page;
+
+use wcf\system\cache\builder\KasMailCacheBuilder;
+use wcf\system\WCF;
+use wcf\util\ArrayUtil;
+
+class KasMailPage extends AbstractPage
+{
+    /**
+     * @inheritDoc
+     */
+    public $neededPermission = ['mod.kas.canSeeMails'];
+
+    /**
+     * List of cached mails
+     * @var array
+     */
+    protected $mails = [];
+
+    /**
+     * @inheritDoc
+     */
+    public function readParameters()
+    {
+        $allowed = ArrayUtil::trim(\explode(
+            "\n",
+            WCF::getSession()->getPermission('mod.kas.mailList')
+        ));
+        foreach (KasMailCacheBuilder::getInstance()->getData() as $mail) {
+            if (!(in_array($mail['mail_login'], $allowed) || in_array($mail['mail_adresses'], $allowed))) {
+                continue;
+            }
+            \array_push($this->mails, $mail);
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function assignVariables()
+    {
+        parent::assignVariables();
+
+        // assign sorting parameters
+        WCF::getTPL()->assign([
+            'mails' => $this->mails
+        ]);
+    }
+}
